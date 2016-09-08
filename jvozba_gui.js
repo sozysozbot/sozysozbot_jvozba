8a1d7759175b6ad298ceeae3db9baff4af1edb8c
sozysobot <sozysozbot@gmail.com>
2016-09-09 05:44:33 +0900

    finished

--- jvozba_gui.js
+++ jvozba_gui.js
@@ -1,3 +1,41 @@
+function jvozba_gui(txt)
+{
+	txt = txt.replace(/h/g, "'");
+	txt = txt.toLowerCase();
+	var arr = txt.split(" ");
+	var candid_arr = get_candidates(arr);
+	var answers = create_every_possibility(candid_arr).map(function(rafsi_list){
+		var result = normalize(rafsi_list);
+		return {lujvo: result.join(""), score: get_lujvo_score(result)};
+	}).sort(function(a,b){
+		return a.score - b.score;
+	});
+	output_answers(answers);
+}
+
+function output_answers(answers)
+{
+	var table = "<table>"
+	for(var i=0; i<answers.length; i++) {
+		var word = answers[i].lujvo;
+		var last = get_info(word[word.length-1]);
+		
+		table += "<tr class='"+ 
+		 (last === "V" ? "brivla" : "cmevla") +"'>";
+			table += "<td>";
+			table += word;
+			table += "</td>";
+			table += "<td>";
+			table += answers[i].score;
+			table += "</td>";
+		table += "</tr>";
+	}
+	
+	table += "</table>"
+	document.getElementById("res").innerHTML = table;
+}
+
+
 /* 
 	create_every_possibility([[1,11], [2], [3,33,333]]) ==> [ [1,2,3],[11,2,3],  [1,2,33],[11,2,33],  [1,2,333],[11,2,333] ]
 	create_every_possibility([[1,11]]) ==> [ [1],[11] ]
