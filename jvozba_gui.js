0972e3267039ff1b480d6efc57007506702aeeab
sozysobot <sozysozbot@gmail.com>
2016-09-09 05:29:14 +0900

    add function create_every_possibility()

--- jvozba_gui.js
+++ jvozba_gui.js
@@ -1,3 +1,26 @@
+/* 
+	create_every_possibility([[1,11], [2], [3,33,333]]) ==> [ [1,2,3],[11,2,3],  [1,2,33],[11,2,33],  [1,2,333],[11,2,333] ]
+	create_every_possibility([[1,11]]) ==> [ [1],[11] ]
+*/
+function create_every_possibility(aa)
+{
+	var arr_arr = JSON.parse(JSON.stringify(aa));
+	if(arr_arr.length === 0) {
+		return [[]];
+	}
+	var arr = arr_arr.pop();
+	
+	var result = [];
+	for(var i=0; i<arr.length; i++) {
+		var e = arr[i];
+		
+		result = result.concat(create_every_possibility(arr_arr).map(function(f){
+			return f.concat([e]);
+		}));
+	}
+	return result;
+}
+	
 function get_candidates(arr)
 {
 	var candid_arr = [];
