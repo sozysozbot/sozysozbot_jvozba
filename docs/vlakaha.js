9b217fb05be6c6de6d39ff966c9de83bf039f2b7
sozysobot <sozysozbot@gmail.com>
2016-09-13 23:38:50 +0900

    think of an outline for vlaka'a

---
+++ docs/vlakaha.js
@@ -0,0 +1,13 @@
+/*
+
+vlakaha("parecivo") --> ["pa", "re", "ci", "vo"]
+vlakaha("fu'ivla") --> ["-fu'i-", "-vla-"]
+vlakaha("pasmabru") --> ["pa", "-sma-", "-bru-"]
+vlakaha("pasymabru") --> ["-pas-", "-mabru-"]
+
+*/
+
+function vlakaha()
+{
+	/* FIXME */
+}
