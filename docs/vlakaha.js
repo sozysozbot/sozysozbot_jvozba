38b3ff4b219f17854fdb5c8a60c95331f4d21563
sozysobot <sozysozbot@gmail.com>
2016-09-14 15:42:02 +0900

    improve the data structure

--- docs/vlakaha.js
+++ docs/vlakaha.js
@@ -1,9 +1,9 @@
 /*
 
 vlakaha("parecivo") --> ["pa", "re", "ci", "vo"]
-vlakaha("fu'ivla") --> ["-fu'i-", "-vla-"]
-vlakaha("pasmabru") --> ["pa", "-sma-", "-bru-"]
-vlakaha("pasymabru") --> ["-pas-", "-mabru-"]
+vlakaha("fu'ivla") --> [["fu'i", "vla"]]
+vlakaha("pasmabru") --> ["pa", ["sma", "bru"]]
+vlakaha("pasymabru") --> [["pas", "mabru"]]
 
 */
 
