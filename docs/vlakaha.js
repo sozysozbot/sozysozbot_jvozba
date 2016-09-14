cf04b7f5ec31c4c75d75493ac252b5912a0dd35a
sozysobot <sozysozbot@gmail.com>
2016-09-14 18:18:36 +0900

    finish? writing jvokaha

--- docs/vlakaha.js
+++ docs/vlakaha.js
@@ -42,26 +42,70 @@ jvokaha2("pasmabru") --> ["pas", "mabru"]
 */
 function jvokaha2(lujvo)
 {
+	var original_lujvo = lujvo;
 	var res = [];
 	while(true){
-		//remove hyphen
-		if(lujvo.charAt(0) === "y") {
-			res.push("y");
-			lujvo = lujvo.slice(1);
-			continue;
-		} else if(lujvo.slice(0,2) === "nr") { // n-hyphen is only allowed before r
-			res.push("n");
-			lujvo = lujvo.slice(1);
-			continue;
-		} else if(lujvo.charAt(0) === "r" && get_CV_info(lujvo.charAt(1)) === "C") { // r followed by a consonant
+		if(lujvo === "") return res;
 		
+		//remove hyphen
+		if(lujvo.charAt(0) === "y" // y-hyphen
+		|| lujvo.slice(0,2) === "nr" // n-hyphen is only allowed before r
+		|| lujvo.charAt(0) === "r" && get_CV_info(lujvo.charAt(1)) === "C" // r followed by a consonant
+		) {
+			res.push(lujvo.charAt(0));
+			lujvo = lujvo.slice(1);
+			continue;
 		}
 		
-		/*
-			FIXME!!!
-		lujvo = shorter_lujvo;
-		res.push(rafsi);
-		*/
+		
+		
+		//drop rafsi from front
+		
+		//CVV can always be dropped
+		if(get_CV_info(lujvo.slice(0,3)) === "CVV"
+		&& ["ai", "ei", "oi", "au"].indexOf(lujvo.slice(1,3))
+		){
+			res.push(lujvo.slice(0,3));
+			lujvo = lujvo.slice(3);
+			continue;
+		}
+		
+		//CV'V can always be dropped
+		if(get_CV_info(lujvo.slice(0,4)) === "CV'V"){
+			res.push(lujvo.slice(0,4));
+			lujvo = lujvo.slice(4);
+			continue;
+		}
+		
+		//CVCCY and CCVCY can always be dropped
+		//how about checking if CC is persimmisble? *FIXME*
+		if(get_CV_info(lujvo.slice(0,5)) === "CVCCY"
+		|| get_CV_info(lujvo.slice(0,5)) === "CCVCY"
+		){
+			res.push(lujvo.slice(0,4));
+			res.push("y");
+			lujvo = lujvo.slice(5);
+			continue;
+		}
+		
+		//the final rafsi can be 5-letter
+		if(get_CV_info(lujvo) === "CVCCV"
+		|| get_CV_info(lujvo) === "CCVCV"
+		){
+			res.push(lujvo);
+			return res;
+		}
+		
+		if(get_CV_info(lujvo.slice(0,3)) === "CVC"
+		|| get_CV_info(lujvo.slice(0,3)) === "CCV"
+		){
+			res.push(lujvo.slice(0,3));
+			lujvo = lujvo.slice(3);
+			continue;
+		}
+	
+		// if all fails...
+		return null;
 	}
 	return res;	
 }
