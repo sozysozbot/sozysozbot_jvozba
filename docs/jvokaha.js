aa783c301707c88200c7b136b1017fe82006af4b
Yanis Lukes <pendrokar@gmail.com>
2019-04-07 17:34:28 +0300

    Added support for GET parameters, which get set as input ("b" & "c") defaults.

--- docs/jvokaha.js
+++ docs/jvokaha.js
@@ -1,6 +1,6 @@
 /*
 jvokaha("fu'ivla") --> ["fu'i", "vla"]
-jvokaha("fu'irvla") --> error // because r-hyphen is unnecessary 
+jvokaha("fu'irvla") --> error // because r-hyphen is unnecessary
 jvokaha("pasymabru") --> ["pas", "y", "mabru"]
 jvokaha("pasmabru") --> error // because {pasmabru} is actually {pa smabru}
 */
@@ -10,9 +10,9 @@ function jvokaha(lujvo)
 	var rafsi_list = arr.filter(function(a){
 		return a.length !== 1; // remove ynr
 	});
-	
+
 	var correct_lujvo = normalize(rafsi_list).join("");  // recreate the lujvo from the rafsi list
-	
+
 	if(lujvo === correct_lujvo) {
 		return arr;
 	} else {
@@ -23,7 +23,7 @@ function jvokaha(lujvo)
 
 /*
 jvokaha2("fu'ivla") --> ["fu'i", "vla"]
-jvokaha2("fu'irvla") --> ["fu'i", "r", "vla"] 
+jvokaha2("fu'irvla") --> ["fu'i", "r", "vla"]
 jvokaha2("pasymabru") --> ["pas", "y", "mabru"]
 jvokaha2("pasmabru") --> ["pas", "mabru"]
 */
@@ -33,9 +33,9 @@ function jvokaha2(lujvo)
 	var res = [];
 	while(true){
 		if(lujvo === "") return res;
-		
+
 		//remove hyphen
-	if(res.length > 0 && res[res.length-1].length !== 1) { // hyphen cannot begin a word; nor can two hyphens 
+	if(res.length > 0 && res[res.length-1].length !== 1) { // hyphen cannot begin a word; nor can two hyphens
 		if(lujvo.charAt(0) === "y" // y-hyphen
 		|| lujvo.slice(0,2) === "nr" // n-hyphen is only allowed before r
 		|| lujvo.charAt(0) === "r" && get_CV_info(lujvo.charAt(1)) === "C" // r followed by a consonant
@@ -44,28 +44,28 @@ function jvokaha2(lujvo)
 			lujvo = lujvo.slice(1);
 			continue;
 		}
-	}	
-		
-		
-		
-		//drop rafsi from front
-		
-		//CVV can always be dropped
-		if(get_CV_info(lujvo.slice(0,3)) === "CVV"
+	}
+
+
+
+	//drop rafsi from front
+
+	//CVV can always be dropped
+	if(get_CV_info(lujvo.slice(0,3)) === "CVV"
 		&& ["ai", "ei", "oi", "au"].indexOf(lujvo.slice(1,3)) !== -1
-		){
+	){
 			res.push(lujvo.slice(0,3));
 			lujvo = lujvo.slice(3);
 			continue;
 		}
-		
+
 		//CV'V can always be dropped
 		if(get_CV_info(lujvo.slice(0,4)) === "CV'V"){
 			res.push(lujvo.slice(0,4));
 			lujvo = lujvo.slice(4);
 			continue;
 		}
-		
+
 		//CVCCY and CCVCY can always be dropped
 		//how about checking if CC is persimmisble? *FIXME*
 		if(get_CV_info(lujvo.slice(0,5)) === "CVCCY"
@@ -76,7 +76,7 @@ function jvokaha2(lujvo)
 			lujvo = lujvo.slice(5);
 			continue;
 		}
-		
+
 		//the final rafsi can be 5-letter
 		if(get_CV_info(lujvo) === "CVCCV"
 		|| get_CV_info(lujvo) === "CCVCV"
@@ -84,7 +84,7 @@ function jvokaha2(lujvo)
 			res.push(lujvo);
 			return res;
 		}
-		
+
 		if(get_CV_info(lujvo.slice(0,3)) === "CVC"
 		|| get_CV_info(lujvo.slice(0,3)) === "CCV"
 		){
@@ -92,10 +92,10 @@ function jvokaha2(lujvo)
 			lujvo = lujvo.slice(3);
 			continue;
 		}
-	
+
 		// if all fails...
 		console.log(res,lujvo)
 		throw new Error("Failed to decompose {" + original_lujvo + "}");
 	}
-	return res;	
+	return res;
 }
